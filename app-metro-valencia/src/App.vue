80969a356fcdc8a71f0f51cd9a987b24b28a160f
Alberto <maestromonteroalberto@gmail.com>
2018-05-15 13:00:14 +0100

    añadido bootstrap

--- app-metro-valencia/src/App.vue
+++ app-metro-valencia/src/App.vue
@@ -1,7 +1,21 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="#"></a>
+          </li>
+          <li>
+            <a href="/"></a>
+          </li>
+          <li></li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <router-view/>
+    </main>
   </div>
 </template>
 
@@ -10,14 +24,3 @@ export default {
   name: 'App'
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
